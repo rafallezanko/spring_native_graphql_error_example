93b8c88f05e6c9c4eff8d6d57751c79e5f29f636
Rafał Leżanko <rafal.lezanko@autenti.com>
2022-12-10 22:12:29 +0100

    springboot in example project upgraded to 3.0.1-SNAPSHOT and works!

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "3.0.0-SNAPSHOT"
+    id("org.springframework.boot") version "3.0.1-SNAPSHOT"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.graalvm.buildtools.native") version "0.9.18"
+    id("org.graalvm.buildtools.native") version "0.9.19"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
 }
